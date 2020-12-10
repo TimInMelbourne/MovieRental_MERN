a053dcb035faa219e439516ab99ebe27ff3a60b0
tim <home@Mac-mini.local>
2020-12-10 17:17:31 +1100

    Save genre to local MongoDB

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ const express = require('express');
 const app = express();
 const genres = require('./routes/genres');
 
+const mongoose = require('mongoose');//load mongoose
+
 app.use(express.json());
 app.use('/api/genres', genres); //load the genres router
 
+//Connect to local mongoDB server
+mongoose.connect('mongodb://localhost/MovieRental', { useNewUrlParser: true, useUnifiedTopology: true , useFindAndModify: false })
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.log('Could not connect to MongoDB', err.message));
 
 app.get('/', (req, res) => {
     res.send('Hello, why not go to /api/genres?');
--- routes/genres.js
+++ routes/genres.js
@@ -1,59 +1,67 @@
 const express = require('express');
 const router = express.Router();
 
+const mongoose = require('mongoose');
+
 const Joi = require('joi');
 router.use(express.json());
 
+const Genre = mongoose.model('Genre', new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 50
+    }
+}));
 
-let genres = [
-    { id: 1, name: 'Action' },
-    { id: 2, name: 'Romantic' },
-    { id: 3, name: 'Comedy' }
-]
-
-
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+    const genres = await Genre.find().sort('name');
     res.send(genres);
 })
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
     const id = req.params.id;
-    const genre = genres.find(g => g.id === parseInt(id));
-    if (!genre) return res.status(404).send(`Genre id ${id} is not found`);
+    const genre = await Genre.find({ _id: id }).catch(err => console.log("something wrong", err.message));
+    if (!genre || genre.length === 0) return res.status(404).send(`Genre id ${id} is not found`);
     res.send(genre);
 })
 
-router.post('/', (req, res) => {
-    const id = genres.length + 1;
+router.post('/', async (req, res) => {
     const { name } = req.body;
+    //1 validate Genre name
     const { error } = validateSchema.validate({ genreName: name });
-    if (!error) {
-        const genre = { id: id, name: name }
-        genres.push(genre);
-        res.send(genre);
-    }
-    else return res.status(400).send(`Genre ${name} is invalid`);
+    if (error) return res.status(400).send(`Genre ${name} is invalid`);
+
+    //2 save to MongoDB
+    var genre = new Genre({ name: name });
+    genre = await genre.save();
+
+    res.send(genre);
 })
 
-router.put('/:id', (req, res) => {
-    const id = req.params.id;
-    const genre = genres.find(g => g.id === parseInt(id));
-    if (!genre) return res.status(404).send(`Genre id ${id} is not found`);
+router.put('/:id', async (req, res) => {
+    //1 validate name
     const { name } = req.body;
     const { error } = validateSchema.validate({ genreName: name });
-    if (!error) {
-        genre.name = name;
-        res.send(genre);
-    }
-    else return res.status(400).send(`Genre ${name} is invalid`);
+    if (error) return res.status(400).send(`Genre ${name} is invalid`);
+
+    const id = req.params.id;
+    var genre = await Genre.findByIdAndUpdate(id, { name: name }, { new: true });
+    //2 find the genre with ID
+    if (!genre || genre.length === 0) return res.status(404).send(`Genre id ${id} is not found`);
+
+    res.send(genre);
 })
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
     const id = req.params.id;
-    const genre = genres.find(g => g.id === parseInt(id));
+    if(id === undefined) return res.status(400).send('Please input a genre id');
+    const genre = await Genre.findByIdAndRemove(id, {useFindAndModify:false});
+
     if (!genre) return res.status(404).send(`Genre id ${id} not found`);
     // genres.pop(genre);
-    genres = genres.filter(g => g.id !== genre.id);
+
     res.send(genre);
 })
 
