53fe1aa40bba2898a06388f76ab604a079a31f61
Tim Niu <timniu2020@gmail.com>
2021-01-12 22:56:57 +1100

    Add rental API

---
+++ models/rental.js
@@ -0,0 +1,80 @@
+const Joi = require('joi');
+const mongoose = require('mongoose');
+
+const Rental = mongoose.model(
+  'Rental',
+  new mongoose.Schema({
+    customer: {
+      type: new mongoose.Schema({
+        name: {
+          type: String,
+          required: true,
+          minlength: 3,
+          maxlength: 50,
+        },
+        isGold: {
+          type: Boolean,
+          default: false,
+        },
+        phone: {
+          type: String,
+          required: true,
+          minlength: 3,
+          maxlength: 50,
+        },
+      }),
+      required: true,
+    },
+    movie: {
+      type: new mongoose.Schema({
+        title: {
+          type: String,
+          required: true,
+          trim: true,
+          minlength: 1,
+          maxlength: 255,
+        },
+        dailyRentalRate: {
+          type: Number,
+          required: true,
+          min: 0,
+          max: 512,
+        },
+      }),
+      required: true,
+    },
+    dateOut: {
+      type: Date,
+      required: true,
+      default: Date.now,
+    },
+    dateReturned: {
+      type: Date,
+    },
+    rentalFee: {
+      type: Number,
+      min: 0,
+    },
+  })
+);
+
+function validateRental(rental) {
+  const schema = Joi.object({
+    customer: Joi.object({
+      _id: Joi.string().required(),
+      name: Joi.string().required(),
+      isGold: Joi.boolean().required(),
+      phone: Joi.string().required(),
+    }),
+    movie: Joi.object({
+      _id: Joi.string().required(),
+      title: Joi.string().required(),
+      dailyRentalRate: Joi.number().required(),
+    }),
+  });
+
+  return schema.validate(rental);
+}
+
+exports.Rental = Rental;
+exports.validate = validateRental;
